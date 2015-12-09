f31495f086e5150f524b64e4838a9b4212ced54f
Aurel-Dragos Hofnar <hofnar@student.tugraz.at>
2015-12-09 02:57:05 +0100

    removed debug output

--- day8/day8-1.py
+++ day8/day8-1.py
@@ -15,6 +15,5 @@ with open("day8.input") as f:
         # replace \x hex representations with a character
         line = re.sub(r'\\x[0-9a-fA-F]{2}', '|', line)
         len_in_mem += len(line) - 2
-        print line, len(line) - 2
 
-print len_code, "-", len_in_mem, "=", len_code - len_in_mem
+print len_code - len_in_mem
